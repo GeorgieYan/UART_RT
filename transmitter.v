92fa95e1ddf72b3ec231e615fb574b8fd6551840
GeorgieYan <46608695+GeorgieYan@users.noreply.github.com>
2023-06-25 23:25:27 -0400

    Update transmitter.v

--- transmitter.v
+++ transmitter.v
@@ -1,37 +1,77 @@
 `timescale 1ns / 1ps
 
-module debounce 
-    #(parameter threshold = 100000)(
+module transmitter(
     input clk, 
-    input btn, 
-    output reg transmit_signal 
+    input reset, 
+    input transmit_signal, //btn signal to trigger the UART communication
+    input [7:0] data, // data transmitted
+    output reg TxD // Transmitter serial output. TxD will be held high during reset, or when no transmissions aretaking place. 
     );
-    
-    reg button1 = 0; //button flip-flop for synchronization. Initialize it to 0 
-    reg button2 = 0; //button flip-flop for synchronization. Initialize it to 0 
-    reg [30:0] count = 0; //20 bits count for increment & decrement when button is pressed or released. Initialize it to 0 
-    
-    // assign button buffer 
-    always @(posedge clk) begin 
-        button1 <= btn; 
-        button2 <= button1; 
-    end
 
+    reg state, next_state, shift, load, clear; // initial & next state variable
+    reg [3:0] bitcounter; //4 bits counter to count up to 10
+    reg [13:0] counter; //14 bits counter to count the baud rate, counter = clock / baud rate
+    reg [9:0] reg_shift; 
+    
+    // transmitter 
     always @(posedge clk) begin 
-        if (button2) begin
-            if (~&count) // NAND
-                count <= count + 1; // add one to counter when button is pressed 
-        end 
-        
-        else begin
-            if (|count) // OR
-                count <= count - 1; // deduct one from counter when button is released 
+        if(reset) begin // reset 
+            state <= 0; 
+            counter <= 0; 
+            bitcounter <= 0; 
         end
-        
-        if (count > threshold) 
-            transmit_signal <= 1; // set debounced signal to 1 if counter is greater than threshold 
-        else 
-            transmit_signal <= 0; // set debounced signal to 0 if counter is smaller than threshold 
-    end
+        else begin
+            counter <= counter + 1; // counter for baud rate generator 
+            if (counter >= 10415) begin // 10415 for 100 MHz Basys 3 board 
+                state <= next_state; 
+                counter <= 0; 
+            	if(load) 
+            	   reg_shift <= {1'b1, data, 1'b0}; // load the data 
+		        if(clear) 
+		           bitcounter <=0; // reset 
+                if(shift) begin 
+                   reg_shift <= reg_shift >> 1; // shift right one bit 
+                   bitcounter <= bitcounter + 1; 
+                end
+            end
+        end
+    end 
+    
+    // state machine 
+    always @(posedge clk) begin 
+        load <= 0; 
+        shift <= 0; 
+        clear <= 0; 
+        TxD <= 1; 
+        case(state)
+            0: begin // idle state
+                if (transmit_signal) begin 
+                    next_state <= 1; // set to transmitting state
+                    load <= 1; // set load to high 
+                    shift <= 0; // no shift 
+                    clear <= 0; // no clearing counter 
+                end 
+		        else begin // back to idle state
+                    next_state <= 0; 
+                    TxD <= 1; 
+                end
+            end
+           
+            1: begin // transmitting state 
+                if (bitcounter >= 10) begin // transmission is complete 
+                    next_state <= 0; // set back to idle state
+                    clear <= 1; // set clear high to clear counters
+                end 
+		        else begin // tranismitting is not complete 
+                    next_state <= 1; // continue in transmitting state 
+                    shift <= 1; // set shift to high 
+                    TxD <= reg_shift[0]; // shift the bit 
+                end
+           end
+           
+           default: next_state <= 0;                      
+        endcase
+end    
 
 endmodule
+
